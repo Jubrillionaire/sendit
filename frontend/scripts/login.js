f45460dcdffd855d51848bc36232ebff1aad3bac
jay <jay@gmail.com>
2019-12-30 23:21:47 +0100

    minor fixes

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,3 +1,55 @@
+// const login = event => {
+//   event.preventDefault();
+
+//   fetch('/api/v1/users/login', {
+//     method: 'POST',
+//     headers: {
+//       'Accept': 'application/json, text/plain, */*',
+//       'Content-type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       email: document.getElementById('email').value,
+//       password: document.getElementById('password').value
+//     })
+//   }).then(res => res.json())
+//     .then(res => {
+//       console.log(res);
+//       if (res.token) {
+//         fetch('/api/v1/me', {
+//             headers:{
+//                 'Authorization': res.token,
+//             }
+//         })
+//         .then(res => res.json())
+//         .then(data => {
+//             if (data.role === 'member'){
+//                 localStorage.setItem('token', res.token);
+//                 localStorage.setItem('userId', res.userId);
+//                 localStorage.setItem('firstname', data.first_name);
+//                 window.location.href = "./userProfile.html";
+//                toastr.success(res.msg)
+//             } else {
+//                 toastr.error('Sorry, only a MEMBER can log in to this page');
+//             }
+//         })
+//         .catch(err => console.log('err occured', err));
+//     } else if (res.msg) {
+//         toastr.error(res.msg)
+//     }
+// }).catch(err => console.log('err occured', err));
+// }
+
+// document.getElementById('login-form').addEventListener('submit', login);
+
+
+// $(document).ready(function(){
+//   $(".hamburger-nav").on("click", function(){
+
+//   $(".first-ul").toggleClass("open");
+
+//   });
+// });
+
 const login = event => {
   event.preventDefault();
 
@@ -27,7 +79,7 @@ const login = event => {
                 localStorage.setItem('userId', res.userId);
                 localStorage.setItem('firstname', data.first_name);
                 window.location.href = "./userProfile.html";
-               toastr.success(res.msg)
+               toastr.success(res.msg);
             } else {
                 toastr.error('Sorry, only a MEMBER can log in to this page');
             }
@@ -41,7 +93,7 @@ const login = event => {
 
 document.getElementById('login-form').addEventListener('submit', login);
 
-
+//handling menu bar
 $(document).ready(function(){
   $(".hamburger-nav").on("click", function(){
 
@@ -49,6 +101,4 @@ $(document).ready(function(){
 
   });
 });
-
-
 
